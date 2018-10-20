8e359bb6e69145f195d8098933c7ec4bcd7418f2
CrazzyGaming <ionut_pustiul.thau@yahoo.com>
2018-10-20 21:55:49 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,5 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-import discord
-from discord.ext import commands
 
 client.on('ready', () => {
     console.log('I am ready!');
