1499561066abed9a5552ac315a25c916c67e3920
CrazzyGaming <ionut_pustiul.thau@yahoo.com>
2018-10-20 21:46:13 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,13 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+import discord
+from discord.ext import commands
 
 client.on('ready', () => {
     console.log('I am ready!');
+    
+TOKEN = 'NTAzMjQyNjc2MzU1ODU4NDMy.Dq0JHA.Y13EmiJZFB9VkZldgUomuBw-x_0'
+client = commands.Bot(command_prefix = 's')
     
 });
 
