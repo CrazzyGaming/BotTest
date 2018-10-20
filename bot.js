0f1dbbacda1ecb6bfe400f43ac85dbcb31b44570
CrazzyGaming <ionut_pustiul.thau@yahoo.com>
2018-10-20 20:20:28 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,21 +11,6 @@ client.on('message', message => {
   	}
 });
 
-client.on('message', message => {
-   if (message.content === 'sjoin') {
-    // Only try to join the sender's voice channel if they are in one themselves
-    if (message.member.voiceChannel) {
-      message.member.voiceChannel.join()
-        .then(connection => { // Connection is an instance of VoiceConnection
-          message.reply('I have successfully connected to the channel!');
-        })
-        .catch(console.log);
-    } else {
-      message.reply('You need to join a voice channel first!');
-    }
-  }
-});
-
 
 
 
