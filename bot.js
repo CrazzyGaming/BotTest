be56250ecd577c30599cfcaa56e1ca38f57323d7
CrazzyGaming <ionut_pustiul.thau@yahoo.com>
2018-10-20 20:57:06 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log('I am ready!');
-    bot.user.setGame("/skemavanguard");
+    client.user.setPresence({ game: { name: '/skemavanguard', type: 0 } })
     
 });
 
