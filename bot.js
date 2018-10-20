f72c102e881b0f26e82d4e2c272473b9a788f86d
CrazzyGaming <ionut_pustiul.thau@yahoo.com>
2018-10-20 21:28:40 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,6 +13,14 @@ client.on('message', message => {
   	}
 });
 
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+    await client.change_presence(game=discord.Game(name="/skemavanguard"))
+
 
 
 
