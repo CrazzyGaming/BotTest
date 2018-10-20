ac6cd2ccfd2e518673b6a463b02a722f090a1ddb
CrazzyGaming <ionut_pustiul.thau@yahoo.com>
2018-10-20 20:44:10 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,5 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-from discord.ext import commands
-from discord.ext.commands import Bot
-from discord.voice_client import VoiceClient
-import asyncio
 
 client.on('ready', () => {
     console.log('I am ready!');
@@ -14,18 +10,6 @@ client.on('message', message => {
     	message.reply('Salut, sunt un Spy Bot creat de DariusOT.');
   	}
 });
-
-bot = commands.Bot(command_prefix="|")
-client = discord.Client()
-
-async def on_ready():
-    print ("Ready")
-
-@bot.command(pass_context=True)
-async def join(ctx):
-    author = ctx.message.author
-    channel = author.voice_channel
-    await bot.join_voice_channel(channel)
 
 
 
